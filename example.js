08b80fc1dc6a3d5222468b5b7133165f16954f03
sirfoga <sirfoga@protonmail.com>
2019-03-13 20:01:43 +0100

    add eslint

--- example.js
+++ example.js
@@ -1,21 +1,15 @@
-'use strict';
-
 const express = require('express');
-const anemoi = require('./lib/anemoi');
 
 const app = express();
 const port = 8080;
 const downloadMap = {
-	"38291": "/home/stefano/_tmp/tok/index.js"
-};  // token -> real location
+  38291: '/home/stefano/_tmp/tok/index.js',
+}; // token -> real location
 
 app.get('/download', (req, res) => {
-	const downloadToken = req.query.token.toString();
-	const realDownload = downloadMap[downloadToken];
-
-	console.log(downloadToken, '->', realDownload);
-
-	res.download(realDownload);
+  const downloadToken = req.query.token.toString();
+  const realDownload = downloadMap[downloadToken];
+  res.download(realDownload);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
