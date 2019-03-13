be1eea081ef08f584d244d4ddacbdbdb77c409af
sirfoga <sirfoga@protonmail.com>
2019-03-13 22:24:06 +0100

    add example

--- example.js
+++ example.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const anemoi = require('./index');
 
 const app = express();
 const port = 8080;
-const downloadMap = {
-  38291: '/home/stefano/_tmp/tok/index.js',
-}; // token -> real location
+
+const retrieveActualLocation = (token) => {
+  const anemoiClient = new anemoi.anemoi.Anemoi();
+  return anemoiClient.get(token);
+};
 
 app.get('/download', (req, res) => {
   const downloadToken = req.query.token.toString();
-  const realDownload = downloadMap[downloadToken];
-  res.download(realDownload);
+  const realDownload = retrieveActualLocation(downloadToken);
+  realDownload.then((val) => {
+    const realPath = `/home/${val}.txt`;
+    res.download(realPath);
+  });
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port);
